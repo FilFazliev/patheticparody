f644e5dfd255d985afc8a9d9488e3129e92d2fc8
FilFazliev <122104246+FilFazliev@users.noreply.github.com>
2023-04-11 18:58:58 +0500

    Update 1 версия.py

--- моя игра/1 версия.py
+++ моя игра/1 версия.py
@@ -19,6 +19,19 @@ clock = pygame.time.Clock()
 
 platform = pygame.image.load('block0.png') 
 
+SPEED = 10
+changeX = 0
+changeY = 0
+
+#персонаж
+
+herostand = pygame.image.load('')
+heror = pygame.image.load('')
+
+hero = herostand
+herorect = heror.get_rect()
+herorect.bottom = HEIGHT-150
+herorect.left = WIDTH-1000
 maps =  [
     '***************************************',
     '*                                     *',
@@ -49,7 +62,35 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    herorect_old = herorect.copy()
 
+
+    platforms = []
+
+    #движение
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        changeX = -1 * SPEED
+        
+
+    if keys[pygame.K_RIGHT]:
+        changeX = SPEED
+        
+
+    if keys[pygame.K_UP]:
+        changeY = -1 * SPEED
+        
+
+    if keys[pygame.K_DOWN]:
+        changeY = SPEED
+        
+
+    if not keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
+        changeY = 0
+
+    if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+        changeX = 0
     
     #генерация платформ
 
@@ -62,7 +103,14 @@ while 1:
                 platformrect.x = 50 * j
                 platformrect.y = 50 * i
                 platforms.append(platformrect)
-                mainScreen.blit(platform, platformrect)       
+                mainScreen.blit(platform, platformrect) 
+                
+    #провера столкновения 
+    for platformrect in platforms:
+        if herorect.colliderect(platformrect) == True:
+            if herorect.left < herorect_old.left:
+                herorect.x -= changeX
+
 
     # заливаем главный фон черным цветом
     mainScreen.fill(mainScreenColor)
