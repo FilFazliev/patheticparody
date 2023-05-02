8105730fcc7144862ee972753a1afc992a802844
Qwerty1145 <arseniyshlapnikov@gmail.com>
2023-05-02 19:59:47 +0500

    ve

--- моя игра/1 версия.py
+++ моя игра/1 версия.py
@@ -19,16 +19,28 @@ clock = pygame.time.Clock()
 SPEED = 30
 changeX = 0
 changeY = 0
+changeX1 = 0
+changeY1 = 0
 
 move = False
 move1 = False
 collision = False
+
+
 #платформы 
+
 thorn = pygame.image.load('шипы.png') 
 border = pygame.image.load('block0.png') 
 platform = pygame.image.load('block0.png') 
 prostr = pygame.image.load('food.png')
 havat = pygame.image.load('havka.png')
+bullet = pygame.image.load('ПУЛЯЯЯ.png')
+
+gun = pygame. Surface((20,20))
+gun.fill(BLACK)
+
+bulletrect = bullet.get_rect()
+
 
 #персонаж
 
@@ -68,7 +80,7 @@ maps =  [
     ],
     [
         '***************************************',
-        '  *-_*----   *                        *',
+        '* *-_*----   *                        *',
         '*-* *****    *                        *',
         '*-*-*     *  *                        *',
         '*-  *      --*                        *',
@@ -99,7 +111,7 @@ maps =  [
         '*                                     *',
         '*                                     *',
         '*                     *******         *',
-        '*                                     *',
+        '*                                     =',
         '*                                     *',
         '*      ******                         *',
         '*                                     *',
@@ -122,28 +134,47 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and move == False :
-                changeX = -1 * SPEED
-                move = True 
-            if event.key == pygame.K_RIGHT and move == False:
-                changeX = SPEED
-                move = True
-                hero = heromover
+        #     if event.key == pygame.K_LEFT and move == False :
+        #         changeX = -1 * SPEED
+        #         move = True 
+        #     if event.key == pygame.K_RIGHT and move == False:
+        #         changeX = SPEED
+        #         move = True
+        #         hero = heromover
             
-            if event.key == pygame.K_UP and move == False:
-                changeY = -1 * SPEED
-                move = True
-                # if collision == True:
-                #     move == False
-            if event.key == pygame.K_DOWN and move == False:
-                changeY = SPEED
-                move = True
+        #     if event.key == pygame.K_UP and move == False:
+        #         changeY = -1 * SPEED
+        #         move = True
+        #         # if collision == True:
+        #         #     move == False
+        #     if event.key == pygame.K_DOWN and move == False:
+        #         changeY = SPEED
+        #         move = True
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
-        elif event.type == pygame.KEYUP:
-            if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN,pygame.K_UP ]:
-                move == False
+        # elif event.type == pygame.KEYUP:
+        #     if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN,pygame.K_UP ]:
+        #         move == False
                 
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        changeX1 = -1 * SPEED
+
+    if keys[pygame.K_RIGHT]:
+        changeX1 = SPEED
+
+    if keys[pygame.K_UP]:
+        changeY1 = -1 * SPEED
+
+    if keys[pygame.K_DOWN]:
+        changeY1 = SPEED
+
+    if not keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
+        changeY1 = 0
+
+    if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+        changeX1 = 0
 
     herorect_old = herorect.copy()
 
@@ -152,9 +183,13 @@ while 1:
     havattt = []
     borders = []
     thorns = []
+    guns = []
+    bullets =[]
 
     herorect.x += changeX
     herorect.y += changeY
+
+
 
     #генерация платформ
 
@@ -192,13 +227,54 @@ while 1:
                 thornRect.y = 50 * i
                 thorns.append(thornRect)
                 mainScreen.blit(thorn, thornRect)
+            if currentmap[i][j] == '=':
+                gunRect = gun.get_rect()
+                gunRect.x = 50 * j
+                gunRect.y = 50 * i
+                guns.append(gunRect)
+                mainScreen.blit(gun, gunRect)
+    
+    #  генерация пуль
+
+
+    # if len(bullets)==0 and time>=180  :
+    
+    if len(bullets)==0:
+        while True:
+            bulletrect = bullet.get_rect()
+            bulletrect.x = gunRect
+            bulletrect.y = gunRect
+            bullets.append(bulletrect)  
+    
+    bulletrect.x += changeX1
+    bulletrect.y += changeY1
+
+    # попадание
+
+    if len(bullets) > 0:
+        for i in range(len(bullets)):
+            if herorect.colliderect(bullets[i]) == True:  
+                time = 0
+                herorect.bottom = HEIGHT - 1000
+                herorect.left = WIDTH-1900
+                break
+
+    # движение пуль 
+
+    # if len(bullets) != 0:
+    #         bulletrectMove = bullet.get_rect()
+    #         bulletrectMove.x -= 10 
+    # if len(bullets) != 0:
+    #         bulletrectMove= bullet.get_rect()
+    #         bulletrectMove.x += 10 
+
 
     score = len(havattt)
 
     #провера столкновения 
+    
     for platformrect in platforms:
         if herorect.colliderect(platformrect) == True:
-            # collision = True
 
              # движемся налево
             if herorect.left < herorect_old.left:
@@ -240,12 +316,19 @@ while 1:
         level +=1
         herorect.bottom = HEIGHT - 1000
         herorect.left = WIDTH-1900
-        
-    if herorect.colliderect(thornRect) == True and level == 1:
-        herorect.bottom = HEIGHT - 1000
-        herorect.left = WIDTH-1900
 
+    for thornRect in thorns:    
+        if herorect.colliderect(thornRect) == True and level == 1:
+            herorect.bottom = HEIGHT - 1000
+            herorect.left = WIDTH-1900
 
+    if len(bullets) > 0:
+        for i in range(len(bullets)):
+            if herorect.colliderect(bullets[i]) == True:  
+                time = 0
+                herorect.bottom = HEIGHT - 1000
+                herorect.left = WIDTH-1900
+                break
 
 
     # заливаем главный фон черным цветом
